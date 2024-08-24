81445d6916abde7aeffd1bb3910a1394636c5a01
Harsh Rajesh Raghamwar <harshraghamwar81@gmail.com>
2024-08-24 16:11:40 +0530

    added icon field in services

--- backend/services/models.py
+++ backend/services/models.py
@@ -6,6 +6,7 @@ class Service(models.Model):
     pricing = models.CharField(max_length=255)
     duration = models.CharField(max_length=50)
     terms_and_conditions = models.TextField()
+    icon_url = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.name
